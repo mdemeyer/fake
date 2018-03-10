b71a4d713bbfef7151a9f7ee3b80bac62ba4c528
Maarten De Meyer <de.meyer.maarten@gmail.com>
2018-03-10 11:05:58 +0100

    Initial concept.

---
+++ README.md
@@ -0,0 +1,7 @@
+# fake
+
+Fake compiler output. Because reasons.
+
+Simple project to play with some new tech:
+* Boost di: http://boost-experimental.github.io/di/
+* c++17
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Scanning dependencies of target fake" << "\n"
+              << "[ 50%] Building CXX object CMakeFiles/fake.dir/src/main.cpp.o" << "\n"
+              << "[100%] Linking CXX executable fake" << "\n"
+              << "[100%] Built target fake" << "\n"
+              << std::endl;
+
+    return 0;
+}
