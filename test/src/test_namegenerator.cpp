a3a949927fe95b47a4984987e16ec430a00b18ce
Maarten De Meyer <de.meyer.maarten@gmail.com>
2018-03-24 08:49:17 +0100

    Test we can reach the ultimate classname.

--- test/src/test_namegenerator.cpp
+++ test/src/test_namegenerator.cpp
@@ -8,3 +8,9 @@ TEST(NameGenerator, get_name)
     NameGenerator gen(123);
     EXPECT_EQ("Transformer", gen.get_name());
 }
+
+TEST(NameGenerator, singleton_bingo)
+{
+    NameGenerator gen(29905798);
+    EXPECT_EQ("SingletonSingletonSingleton", gen.get_name());
+}
